28b0a90538844144feeb04141c7fe96c8a3d85f9
MathewBeldon <mathew.beldon@hotmail.co.uk>
2026-08-16 20:55:33 +0100

    Make questionnaire HTTP test host privilege-independent

--- src/EarlyYearsFoundationRecovery.IntegrationTests/QuestionnaireSubmissionHttpTests.cs
+++ src/EarlyYearsFoundationRecovery.IntegrationTests/QuestionnaireSubmissionHttpTests.cs
@@ -6,6 +6,7 @@ using EarlyYearsFoundationRecovery.Domain.Entities;
 using EarlyYearsFoundationRecovery.Infrastructure.Persistence;
 using EarlyYearsFoundationRecovery.Web.Authentication;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -229,8 +230,16 @@ public sealed class QuestionnaireSubmissionHttpTests : IAsyncLifetime
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            });
             builder.ConfigureServices(services =>
             {
+                services.AddDataProtection()
+                    .UseEphemeralDataProtectionProvider();
+
                 foreach (var descriptor in services
                     .Where(d => d.ServiceType == typeof(ApplicationDbContext)
                         || d.ServiceType == typeof(DbContextOptions)
